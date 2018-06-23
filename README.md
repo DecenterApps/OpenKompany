a7dee8df21efce1a6e4e0d9ee0e97fc4126aef04
Mateja Trifunovski <mtrifunovski@gmail.com>
2018-06-23 23:38:01 +0200

    First webpage

--- README.md
+++
@@ -1,4 +0,0 @@
-# OpenKompany
-Our idea is to be a platform for decentralized and publicly transparent companies. 
-You can add your employees, perform/receive payments and have public and open statistics.
-
--- src/components/Separator/Separator.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export default ({ smallMargin, largeMargin }) => (
-  <div
-    className={`separator ${largeMargin && 'large-margin'} ${smallMargin && 'small-margin'}`}
-  />
-)
